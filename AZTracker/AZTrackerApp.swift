21aa9fdf517a930d523d610b2816bd9ad9080282
AhmedZ70 <101619413+AhmedZ70@users.noreply.github.com>
2025-04-06 14:59:26 -0600

    Initial Commit

---
+++ AZTracker/AZTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AZTrackerApp.swift
+//  AZTracker
+//
+//  Created by Ahmed Zahran on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AZTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
